4d4035359409ee4c6aae6b86e8e9ea89af2194ab
toddnine <toddnine@fefcc46e-18b8-4033-b535-941433f256aa>
2010-05-11 04:58:43 +0000

    Updated config to use correct port
    git-svn-id: https://staff.spidertracks.com/svn/clustering/datanucleus-cassandra/trunk@39 fefcc46e-18b8-4033-b535-941433f256aa

--- src/test/java/me/prettyprint/cassandra/testutils/EmbeddedServerHelper.java
+++ src/test/java/me/prettyprint/cassandra/testutils/EmbeddedServerHelper.java
@@ -12,91 +12,85 @@ import org.apache.cassandra.service.EmbeddedCassandraService;
 import org.apache.thrift.transport.TTransportException;
 
 /**
- * 
- * @author Ran Tavory (rantav@gmail.com)
- * 
- */
+*
+* @author Ran Tavory (rantav@gmail.com)
+*
+*/
 public class EmbeddedServerHelper {
 
-	private static final String TMP = "tmp";
+  private static final String TMP = "tmp";
 
-	private EmbeddedCassandraService cassandra;
+  private EmbeddedCassandraService cassandra;
 
-	/**
-	 * Set embedded cassandra up and spawn it in a new thread.
-	 * 
-	 * @throws TTransportException
-	 * @throws IOException
-	 * @throws InterruptedException
-	 */
-	public void setup() throws TTransportException, IOException,
-			InterruptedException {
-		// delete tmp dir first
-		rmdir(TMP);
-		// make a tmp dir and copy storag-conf.xml and log4j.properties to it
-		copy("/storage-conf.xml", TMP);
-		copy("/log4j.properties", TMP);
-		System.setProperty("storage-config", TMP);
+  /**
+   * Set embedded cassandra up and spawn it in a new thread.
+   *
+   * @throws TTransportException
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  public void setup() throws TTransportException, IOException, InterruptedException {
+    // delete tmp dir first
+    rmdir(TMP);
+    // make a tmp dir and copy storag-conf.xml and log4j.properties to it
+    copy("/storage-conf.xml", TMP);
+    copy("/log4j.properties", TMP);
+    System.setProperty("storage-config", TMP);
 
-		CassandraServiceDataCleaner cleaner = new CassandraServiceDataCleaner();
-		cleaner.prepare();
-		cassandra = new EmbeddedCassandraService();
-		cassandra.init();
-		Thread t = new Thread(cassandra);
-		t.setDaemon(true);
-		t.start();
-	}
+    CassandraServiceDataCleaner cleaner = new CassandraServiceDataCleaner();
+    cleaner.prepare();
+    cassandra = new EmbeddedCassandraService();
+    cassandra.init();
+    Thread t = new Thread(cassandra);
+    t.setDaemon(true);
+    t.start();
+  }
 
-	public void teardown() {
-		CassandraServiceDataCleaner cleaner = new CassandraServiceDataCleaner();
+  public void teardown() {
+    CassandraServiceDataCleaner cleaner = new CassandraServiceDataCleaner();
 
-		try {
-			cleaner.cleanupDataDirectories();
-			rmdir(TMP);
-		} catch (IOException e) {
-			// IGNORE
-		}
-	}
+    try {
+      cleaner.cleanupDataDirectories();
+      rmdir(TMP);
+    } catch (IOException e) {
+      // IGNORE
+    }
+  }
 
-	private static void rmdir(String dir) throws IOException {
-		File dirFile = new File(dir);
-		if (dirFile.exists()) {
-			FileUtils.deleteDir(new File(dir));
-		}
-	}
+  private static void rmdir(String dir) throws IOException {
+    File dirFile = new File(dir);
+    if (dirFile.exists()) {
+      FileUtils.deleteDir(new File(dir));
+    }
+  }
+  /**
+   * Copies a resource from within the jar to a directory.
+   *
+   * @param resource
+   * @param directory
+   * @throws IOException
+   */
+  private static void copy(String resource, String directory) throws IOException {
+    mkdir(directory);
+    InputStream is = EmbeddedServerHelper.class.getResourceAsStream(resource);
+    String fileName = resource.substring(resource.lastIndexOf("/") + 1);
+    File file = new File(directory + System.getProperty("file.separator") + fileName);
+    OutputStream out = new FileOutputStream(file);
+    byte buf[] = new byte[1024];
+    int len;
+    while ((len = is.read(buf)) > 0) {
+      out.write(buf, 0, len);
+    }
+    out.close();
+    is.close();
+  }
 
-	/**
-	 * Copies a resource from within the jar to a directory.
-	 * 
-	 * @param resource
-	 * @param directory
-	 * @throws IOException
-	 */
-	private static void copy(String resource, String directory)
-			throws IOException {
-		mkdir(directory);
-		InputStream is = EmbeddedServerHelper.class
-				.getResourceAsStream(resource);
-		String fileName = resource.substring(resource.lastIndexOf("/") + 1);
-		File file = new File(directory + System.getProperty("file.separator")
-				+ fileName);
-		OutputStream out = new FileOutputStream(file);
-		byte buf[] = new byte[1024];
-		int len;
-		while ((len = is.read(buf)) > 0) {
-			out.write(buf, 0, len);
-		}
-		out.close();
-		is.close();
-	}
-
-	/**
-	 * Creates a directory
-	 * 
-	 * @param dir
-	 * @throws IOException
-	 */
-	private static void mkdir(String dir) throws IOException {
-		FileUtils.createDirectory(dir);
-	}
+  /**
+   * Creates a directory
+   * @param dir
+   * @throws IOException
+   */
+  private static void mkdir(String dir) throws IOException {
+    FileUtils.createDirectory(dir);
+  }
 }
